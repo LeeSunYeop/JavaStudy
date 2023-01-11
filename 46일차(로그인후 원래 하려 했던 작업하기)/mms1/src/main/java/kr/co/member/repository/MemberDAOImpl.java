234148bf20df9d86a0a6d56d49050666dc91f033
SunYeop <lssy210@gmail.com>
2023-01-11 19:38:12 +0900

    46일차 업로드

---
+++ 46일차(로그인후 원래 하려 했던 작업하기)/mms1/src/main/java/kr/co/member/repository/MemberDAOImpl.java
@@ -0,0 +1,63 @@
+package kr.co.member.repository;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import kr.co.member.domain.MemberDTO;
+
+@Repository
+public class MemberDAOImpl implements MemberDAO{
+	
+	@Autowired
+	private SqlSession session;
+	private final String NS = "kr.co.member";
+	
+
+	@Override
+	public void insert(MemberDTO dto) {
+		
+		session.insert(NS+".insert", dto);		
+	}
+
+
+	@Override
+	public MemberDTO read(String id) {
+		
+		return session.selectOne(NS+".read", id);
+	}
+
+
+	@Override
+	public void update(MemberDTO dto) {
+		
+		session.update(NS+".update", dto);
+		
+	}
+
+
+	@Override
+	public int changePw(Map<String, Object> map) {
+		
+		return session.update(NS+".changePw", map);
+	}
+
+
+	@Override
+	public MemberDTO login(MemberDTO login) {
+		
+		return session.selectOne(NS+".login", login);
+	}
+
+
+
+	@Override
+	public void delete(MemberDTO dto) {
+		
+		session.delete(NS+".delete", dto);
+		
+	}
+
+}
