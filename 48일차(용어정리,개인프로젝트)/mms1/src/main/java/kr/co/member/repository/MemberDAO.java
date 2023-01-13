a3c21249a2a4d597d61685c8b87895c34e45907b
SunYeop <lssy210@gmail.com>
2023-01-13 19:56:25 +0900

    48일차 업로드

---
+++ 48일차(용어정리,개인프로젝트)/mms1/src/main/java/kr/co/member/repository/MemberDAO.java
@@ -0,0 +1,21 @@
+package kr.co.member.repository;
+
+import java.util.Map;
+
+import kr.co.member.domain.MemberDTO;
+
+public interface MemberDAO {
+
+	void insert(MemberDTO dto);
+
+	MemberDTO read(String id);
+
+	void update(MemberDTO dto);
+
+	int changePw(Map<String, Object> map);
+
+	MemberDTO login(MemberDTO login);
+	
+	void memberDelete(MemberDTO dto);
+
+}
